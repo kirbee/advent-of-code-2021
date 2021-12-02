64a62a773ccfa9581bc95954b60c54f1220a3ebc
Kirbee Parsons <kparsons@wealthsimple.com>
2021-12-01 18:50:56 -0800

    setup

---
+++ index.js
@@ -0,0 +1,60 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`What day do you want to create? [01-24]`, (dateString) => {
+  const date = parseInt(dateString, 10);
+  if (date > 0 || date < 24) {
+    createFiles(date < 10 ? `0${date}` : date);
+  } else {
+    throw new Error('Invalid date');
+  }
+  rl.close();
+});
+
+const template = `
+import { readInput } from '../utils.js';
+
+const exampleInput = readInput('./exampleInput.txt');
+const givenInput = readInput('./givenInput.txt');
+
+const partOneSolution = () => {
+  // Do something
+}
+
+const partTwoSolution = () => {
+  // Do something
+}
+
+
+console.log('Part one solution:', partOneSolution);
+console.log('Part two solution:', partTwoSolution);`;
+
+const fileErrorHandler = (err) => {
+  if (err) {
+    console.log(err);
+    throw new Error(err);
+  } else {
+    console.log('File written successfully\n');
+  }
+};
+
+const createFiles = async (path) => {
+  const folderName = `./${path}`;
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+
+      fs.writeFile(`${folderName}/solution.js`, template, fileErrorHandler);
+      ['exampleInput', 'givenInput'].forEach((item) =>
+        fs.writeFile(`${folderName}/${item}.txt`, '', fileErrorHandler)
+      );
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
---
+++ utils.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import readline from 'readline';
+
+export async function readInput(path, options = { parseNumber: false }) {
+  const { parseNumber } = options;
+  const result = [];
+  const fileStream = fs.createReadStream(path);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  for await (const line of rl) {
+    result.push(line);
+  }
+  return parseNumber ? result.map((r) => parseInt(r, 10)) : r;
+}
